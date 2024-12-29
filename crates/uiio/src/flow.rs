e201328a6d74c581bfbeafe73955e87b25f3f701
Denis Kolodin <deniskolodin@gmail.com>
2024-12-29 20:23:46 +0100

    Add the Flow

--- crates/uiio/src/flow.rs
+++ crates/uiio/src/flow.rs
@@ -1,5 +1,34 @@
-use serde::Serialize;
+use derive_more::{From, Into};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::any::type_name;
+
+#[derive(Clone, From, Into, Serialize, Deserialize, PartialEq, Eq)]
+pub struct PackedData(pub Vec<u8>);
+
+pub trait DataFraction
+where
+    Self: DeserializeOwned + Serialize,
+    Self: Clone + Sync + Send + 'static,
+{
+}
+
+impl<T> DataFraction for T
+where
+    T: DeserializeOwned + Serialize,
+    T: Clone + Sync + Send + 'static,
+{
+}
+
+pub trait Flow: DataFraction {
+    type Action: DataFraction;
+    type Event: DataFraction;
+
+    fn class() -> &'static str {
+        type_name::<Self>()
+    }
+
+    fn apply(&mut self, event: Self::Event);
+}
 
 pub trait OutFlow: Default + Serialize {
     // TODO: No serialize needed
