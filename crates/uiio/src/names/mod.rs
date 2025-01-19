77b415a3b4b83a71ea07ca37d57c98e22934e38f
Denis Kolodin <deniskolodin@gmail.com>
2025-01-19 12:31:57 +0100

    Add the full name builder macro

--- crates/uiio/src/names/mod.rs
+++ crates/uiio/src/names/mod.rs
@@ -1,3 +1,4 @@
+pub mod extension;
 mod fqn;
 mod id;
 mod pqn;
--- crates/uiio/src/names/pqn.rs
+++ crates/uiio/src/names/pqn.rs
@@ -1,21 +1,5 @@
 use crate::names::Fqn;
 
-#[macro_export]
-macro_rules! name {
-    ($($item:expr),* $(,)?) => {{
-        let components = vec![$($item.to_string()),*];
-        $crate::names::Pqn { components }
-    }};
-}
-
-#[macro_export]
-macro_rules! partial {
-    ($text:expr) => {{
-        let components = const_str::split!($text, '.');
-        $crate::names::Pqn::from_iter(components)
-    }};
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Pqn {
     components: Vec<String>,
@@ -39,27 +23,5 @@ impl Pqn {
 impl From<Pqn> for Fqn {
     fn from(pqn: Pqn) -> Self {
         pqn.into_full()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use anyhow::Result;
-
-    #[test]
-    fn test_name_macro() -> Result<()> {
-        let expected = "one.two".parse()?;
-        let name = Fqn::from(name!("one", "two"));
-        assert_eq!(name, expected);
-        Ok(())
-    }
-
-    #[test]
-    fn test_partial_macro() -> Result<()> {
-        let expected = "one.two".parse()?;
-        let name = Fqn::from(partial!("one.two"));
-        assert_eq!(name, expected);
-        Ok(())
     }
 }
