9c60cb9d015844d0fb0c522bb6cf207725b3f4cf
Denis Kolodin <deniskolodin@gmail.com>
2025-01-21 12:25:24 +0100

    Derive Debug for records

--- crates/uiio/src/protocol.rs
+++ crates/uiio/src/protocol.rs
@@ -3,13 +3,13 @@ use crate::names::{FlowId, Fqn};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 pub struct Record<'a, T: EventFlow> {
     pub id: FlowId,
     pub event: Event<'a, T>,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum Event<'a, T: EventFlow> {
     Create { fqn: &'a Fqn, class: &'a str },
@@ -17,13 +17,13 @@ pub enum Event<'a, T: EventFlow> {
     Destroy,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct RecordDe {
     pub id: FlowId,
     pub event: EventDe,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum EventDe {
     Create { fqn: Fqn, class: String },
