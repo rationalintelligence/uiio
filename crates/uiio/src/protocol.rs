f27e1083591c3b3c6fd022ad9337ceb9d24e4a39
Denis Kolodin <deniskolodin@gmail.com>
2025-01-21 16:56:18 +0100

    Cloneable events

--- crates/uiio/src/protocol.rs
+++ crates/uiio/src/protocol.rs
@@ -3,13 +3,13 @@ use crate::names::{FlowId, Fqn};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, Clone)]
 pub struct Record<'a, T: EventFlow> {
     pub id: FlowId,
     pub event: Event<'a, T>,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum Event<'a, T: EventFlow> {
     Create { fqn: &'a Fqn, class: &'a str },
@@ -17,13 +17,13 @@ pub enum Event<'a, T: EventFlow> {
     Destroy,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct RecordDe {
     pub id: FlowId,
     pub event: EventDe,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum EventDe {
     Create { fqn: Fqn, class: String },
